1f0e343dc253c0109453a775725c555d9740e6d7
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-06-10 19:01:25 +0530

    Hibernate Intro

---
+++ HibernateBasics/src/main/java/org/vinaylogics/hibernatebasics/xmlconfiguration/EmployeeXMLTest.java
@@ -0,0 +1,28 @@
+package org.vinaylogics.hibernatebasics.xmlconfiguration.models;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.io.File;
+
+public class EmployeeXMLTest {
+    public static void main(String[] args) {
+        File file = new File(EmployeeXMLTest.class.getClassLoader().getResource("hibernate.cfg.xml").getFile());
+        SessionFactory sessionFactory = new Configuration().configure(file)
+                .buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction t = session.beginTransaction();
+        Employee employee = new Employee();
+        employee.setId(102);
+        employee.setFirstName("Saini");
+        employee.setLastName("Supreet");
+
+        session.save(employee);
+        t.commit();
+        System.out.println("Save Successful");
+        sessionFactory.close();
+        session.close();
+    }
+}
