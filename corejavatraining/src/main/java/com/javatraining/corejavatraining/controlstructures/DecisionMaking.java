eecfcbab02b05c51305f8ffc622940a18114849a
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-06-12 11:00:45 +0530

    Control Strutures Complete

---
+++ corejavatraining/src/main/java/com/javatraining/corejavatraining/controlstructures/DecisionMaking.java
@@ -0,0 +1,70 @@
+package com.javatraining.corejavatraining.controlstructures;
+
+public class DecisionMaking {
+
+    public static void main(String[] args) {
+        checkIfCondition();
+        checkIfElseBlockCondition();
+        checkIfElseIfBlock();
+        checkSwitchCase();
+    }
+
+    private static void checkSwitchCase() {
+        String day = "Tue";
+        switch (day) {
+            case "Mon":
+                System.out.println("Monday");
+                break;
+            case "Tue":
+                System.out.println("Tuesday");
+                break;
+            case "Wed":
+                System.out.println("Wednesday");
+                break;
+            case "Thu":
+                System.out.println("Thursday");
+                break;
+            case "Fri":
+                System.out.println("Friday");
+                break;
+            case "Sat":
+                System.out.println("Saturday");
+                break;
+            case "Sun":
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println(" Wrong Day chosen");
+
+        }
+    }
+
+    private static void checkIfElseIfBlock() {
+        int a = 10;
+        if (a < 5) {
+            System.out.println("A is less than 5");
+        } else if (a > 5 && a < 10) {
+            System.out.println("A is greater than 5 and less than 10");
+        } else {
+            System.out.println(" A is 10");
+        }
+    }
+
+    private static void checkIfElseBlockCondition() {
+        if (true) {
+            System.out.println("If else if block executed");
+        } else {
+            System.out.println("If else else block executed");
+        }
+    }
+
+    private static void checkIfCondition() {
+        if (true) {
+            System.out.println("If true");
+        }
+
+        if (false) {
+            System.out.println("Never executed block");
+        }
+    }
+}
---
+++ corejavatraining/src/main/java/com/javatraining/corejavatraining/controlstructures/IteratingCS.java
@@ -0,0 +1,43 @@
+package com.javatraining.corejavatraining.controlstructures;
+
+public class IteratingCS {
+
+    public static void main(String[] args) {
+        System.out.println("======================== For Loop ===========================");
+        checkForLoop();
+        System.out.println("====================While Loop=====================");
+        checkForWhileLoop();
+        System.out.println("====================do While Loop=====================");
+        checkDoWhileLoop();
+    }
+
+    private static void checkDoWhileLoop() {
+        boolean queueAvailable = true;
+        int i = 0;
+        do{
+            System.out.println(i);
+            if( ++i > 10){
+                queueAvailable = false;
+            }
+        }while (queueAvailable);
+    }
+
+    private static void checkForWhileLoop() {
+        boolean queueAvailable = true;
+        int i = 0;
+        while (queueAvailable){
+            System.out.println(i);
+            if( ++i > 10){
+                queueAvailable = false;
+            }
+
+        }
+    }
+
+    private static void checkForLoop() {
+
+        for (int i = 1; i <= 10;i++){
+            System.out.println(i);
+        }
+    }
+}
