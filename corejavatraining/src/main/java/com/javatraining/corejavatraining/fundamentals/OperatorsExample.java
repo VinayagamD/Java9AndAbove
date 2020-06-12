6bb783b2923850734ac21214243eb24911a9d2e2
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-06-12 10:16:18 +0530

    Operators Complete

--- corejavatraining/src/main/java/com/javatraining/corejavatraining/fundamentals/OperatorsExample.java
+++ corejavatraining/src/main/java/com/javatraining/corejavatraining/fundamentals/OperatorsExample.java
@@ -1,4 +1,52 @@
 package com.javatraining.corejavatraining.fundamentals;
 
-public class Operators {
+public class OperatorsExample {
+
+    public static void main(String[] args) {
+//        Arithmetic Operator
+        System.out.println("====================== Arithmetic Operator ============================");
+        System.out.println(2 + 3);
+        System.out.println(3-2);
+        System.out.println(3*2);
+        System.out.println(3/2);
+        System.out.println(4/2);
+        System.out.println(4%2);
+        System.out.println(3%2);
+        System.out.println("====================== Relational Operator ============================");
+//        Relational Operator
+        System.out.println(1 == 1);
+        System.out.println(1 != 1);
+        System.out.println(1 < 2);
+        System.out.println(1 > 2);
+        System.out.println(1 <= 2);
+        System.out.println(1 <= 1);
+        System.out.println(1 < 1);
+        System.out.println(1 >= 1);
+        System.out.println(1 >= 2);
+        System.out.println("====================== Logical Operator ============================");
+//        Logical Operator
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(false && true);
+        System.out.println(false && false);
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println(false || false);
+        System.out.println(!true);
+        System.out.println(!false);
+        System.out.println("====================== Bitwise Operator ============================");
+//        Bitwise Operator
+        System.out.println(2&6);
+        System.out.println(2|4);
+        System.out.println(2^4);
+        System.out.println(4^6);
+        System.out.println(~4);
+        System.out.println(4 <<1); // 0 0 0 0
+        System.out.println(4 <<3); // 0 0 0 0
+        System.out.println(4 >>> 3); // 0 0 0 0
+        System.out.println(4 >>1);
+        System.out.println(4 >>3);
+
+    }
 }
