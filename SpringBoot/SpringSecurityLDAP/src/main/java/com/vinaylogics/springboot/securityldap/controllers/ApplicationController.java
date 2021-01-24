d96559b00d9cf6326e1b0fb5a7f86cc78caeebd0
VinayagamD <vinayagam.d.ganesh@gmail.com>
2021-01-25 00:36:58 +0530

    Spring Security LDAP Demo

---
+++ SpringBoot/SpringSecurityLDAP/src/main/java/com/vinaylogics/springboot/securityldap/controllers/ApplicationController.java
@@ -0,0 +1,15 @@
+package com.vinaylogics.springboot.securityldap.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ldap")
+public class ApplicationController {
+
+    @GetMapping("/security")
+    public String securityMethod(){
+        return "secure rest endpoints with ldap";
+    }
+}
---
+++ SpringBoot/SpringSecurityLDAP/src/test/java/com/vinaylogics/springboot/securityldap/SpringSecurityLdapApplicationTest.java
@@ -0,0 +1,15 @@
+package com.vinaylogics.springboot.securityldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
